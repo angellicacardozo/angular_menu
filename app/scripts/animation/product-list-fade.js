552294e1ebad6f59a92545755e8560809d1720b8
Angéllica Cardozo <angellica.c.a@gmail.com>
2015-10-11 11:22:44 -0300

    Remove useless route

--- app/scripts/animation/product-list-fade.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	console.log(angular.element('.append-product'));
-
-	$('.append-product').on('click', function() {
-		console.log('Click');
-
-		return false;
-	});
-});
